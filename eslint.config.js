c5ab44d14d0c0959d4bbc280360a9f7f83ee9fdf
Sara-mostafa200 <117530120+Sara-mostafa200@users.noreply.github.com>
2025-08-19 08:54:21 +0300

    chore: setup eslint, prettier, clsx/classnames, twMerge, tailwindcss, shadcn/ui

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    ignores: ['dist', 'node_modules'],
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
+    plugins: {
+      react,
+    },
+
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      eqeqeq: ['error', 'always'],
+      curly: 'error',
+      'no-console': 'warn',
+
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'react/self-closing-comp': 'warn',
+
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-])
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Button } from './components/ui/button';
+import { cn } from './lib/utils';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const sara = false;
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={cn('bg-red-500', sara && 'bg-blue-500')}>
+      sara
+      <Button>sara and shrouq</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
